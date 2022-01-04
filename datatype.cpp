898267fe3b95fcd2a3cb42ac43c34cd8363f72f4
Shruti kumari <87003332+123soniyo@users.noreply.github.com>
2022-01-04 23:53:38 +0530

    Add files via upload

---
+++ datatype.cpp
@@ -0,0 +1,49 @@
+// APNA COLLEGE
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a; //declaration
+//     a = 12; //intialisation
+
+//     cout<<"size of int"<<sizeof(a)<<endl;
+
+//     float b;
+//     cout<<"size of float"<<sizeof(b)<<endl;
+
+//     char c;
+//     cout<<"size of char"<<sizeof(c)<<endl;
+
+//     bool d;
+//     cout<<"size of bool"<<sizeof(d)<<endl;
+
+//     short int si;
+//     long int li;
+//     cout<<"size of short int"<<sizeof(si)<<endl;
+//     cout<<"size of long int"<<sizeof(li)<<endl;
+     
+    
+//     return 0;
+// }
+
+
+//HARRY
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 2;
+    float b = 2.3;
+    char c= 'g';
+
+    cout<<"the valu of int is" <<a;
+    cout<<"\n the value of float is" <<b;
+    cout<<"\nthe value of c is" <<c;
+    
+    return 0;
+
+
+}
